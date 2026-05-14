9cc2ad994218dc25ca917a2ee55121e71f1c12f4
Legendnt1 <legende.e@hotmail.com>
2026-05-14 11:43:20 -0500

    refactor(config): update CORS configuration to allow requests from new frontend origin

--- src/main/java/com/hampcoders/electrolink/shared/configuration/WebConfig.java
+++ src/main/java/com/hampcoders/electrolink/shared/configuration/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
  * WebConfig is a configuration class that sets up Cross-Origin Resource Sharing (CORS)
  * for the application. It allows requests from the specified origin
- * (http://localhost:4200) and permits various HTTP methods and headers.
+ * (http://localhost:8081) and permits various HTTP methods and headers.
  * This configuration is essential for enabling communication between the frontend
  * (e.g., an Angular application) and the backend API without encountering CORS issues.
  */
@@ -26,7 +26,7 @@ public class WebConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200")
+            .allowedOrigins("http://localhost:8081/swagger-ui/index.html")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*");
       }
