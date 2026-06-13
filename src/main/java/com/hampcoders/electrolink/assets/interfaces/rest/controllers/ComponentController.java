e1ed94d95455720737f538355f13797902c81d70
Legendnt1 <legende.e@hotmail.com>
2026-06-13 00:20:25 -0500

    refactor(controller): remove unnecessary blank line in ComponentController

--- src/main/java/com/hampcoders/electrolink/assets/interfaces/rest/controllers/ComponentController.java
+++ src/main/java/com/hampcoders/electrolink/assets/interfaces/rest/controllers/ComponentController.java
@@ -19,7 +19,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
