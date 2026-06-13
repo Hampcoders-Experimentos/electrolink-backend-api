4180f8c81bce3050225f24f30896ee4c45fa95f3
Legendnt1 <legende.e@hotmail.com>
2026-06-13 18:56:20 -0500

    chore: rename WebConfig.java and update package structure for better organization

--- src/main/java/com/hampcoders/electrolink/shared/infrastructure/documentation/openapi/cors/WebConfig.java
+++ src/main/java/com/hampcoders/electrolink/shared/infrastructure/documentation/openapi/cors/WebConfig.java
@@ -1,4 +1,4 @@
-package com.hampcoders.electrolink.shared.configuration;
+package com.hampcoders.electrolink.shared.infrastructure.documentation.openapi.cors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
